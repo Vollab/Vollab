af5f714895ff28c7792e651c5fcbf0d5b00e3f1e
InSTinToS <miguelandradebarreto2@gmail.com>
2023-10-22 21:19:21 -0300

    :label: adds CandidateRequest type

--- common/types/events/event.ts
+++ common/types/events/event.ts
@@ -1,6 +1,6 @@
-import { Subjects } from './subjects'
+import { Subjects } from "./subjects";
 
 export interface Event<S extends Subjects = any, T extends object = any> {
-	subject: S
-	data: T
+  subject: S;
+  data: T;
 }
---
+++ common/types/signup/candidate.ts
@@ -0,0 +1,10 @@
+export interface CandidateRequest {
+  name: string;
+  email: string;
+  phone: string;
+  avatar?: string;
+  password: string;
+  biography: string;
+  activity_areas: string[];
+  links?: { title: string; link: string }[];
+}
