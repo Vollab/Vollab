c088d957622cefa23c28bff756d0cc8b407d3a56
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-11-09 21:05:29 -0300

    ✏️ fixes `http code` at `conflict error`

--- common/errors/conflict.ts
+++ common/errors/conflict.ts
@@ -1,7 +1,7 @@
 import { CustomError } from './custom'
 
 export class ConflictError extends CustomError {
-	status_code = 423
+	status_code = 409
 
 	constructor(message?: string) {
 		super(message || 'Conflict')
