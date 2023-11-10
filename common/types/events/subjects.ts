75f4bfa775a9c5ba2a2a582465b3dd77292ac9fc
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-11-09 21:11:49 -0300

    🏷️ creates `enrollment status` and `vacancy work mode`

--- common/types/events/subjects.ts
+++ common/types/events/subjects.ts
@@ -1,11 +1,17 @@
 export enum Subjects {
 	OrdererCreated = 'Orderer.Created',
 	OrdererUpdated = 'Orderer.Updated',
+	OrdererDeleted = 'Orderer.Deleted',
 	Orderer = 'Orderer.>',
 	CandidateCreated = 'Candidate.Created',
 	CandidateUpdated = 'Candidate.Updated',
+	CandidateDeleted = 'Candidate.Deleted',
 	Candidate = 'Candidate.>',
 	CandidateAreaCreated = 'CandidateArea.Created',
 	CandidateAreaDeleted = 'CandidateArea.Deleted',
-	CandidateArea = 'CandidateArea.>'
+	CandidateArea = 'CandidateArea.>',
+	DemandCreated = 'Demand.Created',
+	DemandUpdated = 'Demand.Updated',
+	DemandDeleted = 'Demand.Deleted',
+	Demand = 'Demand.>'
 }
