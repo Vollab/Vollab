fc580b1b27d515f8517fb56257e6ed967ebbade0
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2023-11-09 21:05:59 -0300

    🔥 removes `consumers`

--- common/global/consumers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './orderer/created'
-export * from './orderer/updated'
-export * from './candidate/created'
-export * from './candidate/updated'
-export * from './candidate-area/created'
-export * from './candidate-area/deleted'
